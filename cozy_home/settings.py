ce2802027da81bbf150c480dcc02ca550f3374ec
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-09-10 16:14:54 +0000

    Deploy to Heroku

--- cozy_home/settings.py
+++ cozy_home/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-sfl+%@z4#6-w$#e#0t%jfyyc7dy2e9&+cuo@zms1ob)#4jlyju
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-konstanciaa-cozyhome-z6q9ff9n6po.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-konstanciaa-cozyhome-z6q9ff9n6po.ws-eu104.gitpod.io', 'my-cozy-home-c15fa3106139.herokuapp.com']
 
 
 # Application definition
@@ -119,12 +120,17 @@ WSGI_APPLICATION = 'cozy_home.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
